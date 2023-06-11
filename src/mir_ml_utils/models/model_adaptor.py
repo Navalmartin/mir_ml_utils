9d5ed8a50d8ee11709eb5c988d927b4ddbfb7b69
pockerman <a.giavaras@gmail.com>
2023-06-11 09:51:46 +0100

    Add doc string

--- src/mir_ml_utils/models/model_adaptor.py
+++ src/mir_ml_utils/models/model_adaptor.py
@@ -11,11 +11,22 @@ class AdaptorBase(object):
 
 
 class AddLinearLayerAdaptor(AdaptorBase):
+    """Add a linear layer to the given model. The layer can either
+    be the LAST_LAYER in the network in which case this class adds a linear
+    with dimensions (num_ftrs, num_ftrs) or by indicating the size of the output of the layer
+
+    """
 
     def __init__(self, params: dict):
         super(AddLinearLayerAdaptor, self).__init__(params=params)
 
     def add_linear_layer(self, model_ft: nn.Module) -> nn.Module:
+
+        if 'out' not in self.params:
+            raise ValueError("The self.params is missing the 'out' keyword. "
+                             "This should specify either 'LAST_LAYER' or the size of the"
+                             "output of the layer")
+
         num_ftrs = model_ft.fc.in_features
         if self.params['out'] == 'LAST_LAYER':
             model_ft.fc = nn.Linear(num_ftrs, num_ftrs)
